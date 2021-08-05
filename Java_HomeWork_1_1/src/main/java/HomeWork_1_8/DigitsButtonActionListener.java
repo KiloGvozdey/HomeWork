3af9d444babab129f82f51d8deaa077239755a7e
Ilya <79869848545@mail.ru>
2021-08-05 05:07:39 +0400

    Добавлены файлы с ДЗ по 8-му уроку

---
+++ Java_HomeWork_1_1/src/main/java/HomeWork_1_8/DigitsButtonActionListener.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DigitsButtonActionListener implements ActionListener {
+    private final JTextField textField;
+    private final StringBuilder sb;
+
+    public DigitsButtonActionListener(JTextField textField) {
+        this.textField = textField;
+        sb = new StringBuilder();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton btn = (JButton) e.getSource();
+        sb.append(textField.getText()).append(btn.getText());
+        textField.setText(sb.toString());
+        sb.setLength(0);
+    }
+}
