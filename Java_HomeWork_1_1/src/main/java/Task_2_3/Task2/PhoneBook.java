b405579ea0a8eeeb8b75bcfb4a61f240aa6111cd
Ilya <79869848545@mail.ru>
2021-10-24 16:36:15 +0400

    Добавлены задания 3,4,5 для 2-го уровня Java

---
+++ Java_HomeWork_1_1/src/main/java/Task_2_3/Task2/PhoneBook.java
@@ -0,0 +1,27 @@
+package Task_2_3.Task2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneBook {
+    private final Map<String, ArrayList<Integer>>phoneNumberList = new HashMap<>();
+
+
+    public void add(String surname, int phoneNumber){
+        for (String s : phoneNumberList.keySet()) {
+            if(s.equals(surname)){
+                ArrayList<Integer> list= phoneNumberList.get(surname);
+                list.add(phoneNumber);
+                phoneNumberList.put(s, list);
+                return;
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(phoneNumber);
+        phoneNumberList.put(surname, list);
+    }
+    public ArrayList<Integer> get(String surname){
+        return phoneNumberList.getOrDefault(surname, null);
+    }
+}
