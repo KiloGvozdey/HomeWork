be0bd4e106a564bc2a93d123f5f825b5c02a7938
Ilya <79869848545@mail.ru>
2021-11-09 10:58:29 +0400

    Юобавлены файлы с домашкой по 8-му уроку 2-го уровня

---
+++ Java_HomeWork_1_1/src/main/java/Task_2_8/Server/AuthService.java
@@ -0,0 +1,49 @@
+package Task_2_8.Server;
+
+import java.util.Optional;
+import java.util.Set;
+
+public class AuthService {
+    private static final Set<User>users = Set.of(
+            new User("user1", "l1", "p1"),
+            new User("user2", "l2", "p2"),
+            new User("user3", "l3", "p3")
+    );
+
+    public Optional<String> findUsernameByLoginAndPassword(String login, String password) {
+
+      return users.stream()
+              .filter(user -> (user.getLogin().equals(login)) && (user.getPassword()
+              .equals(password)))
+              .findFirst()
+              .map(User::getUsername);
+    }
+
+
+
+   private static class User{
+        private String username;
+        private String login;
+        private String password;
+
+        public User(String username, String login, String password) {
+            this.username = username;
+            this.login = login;
+            this.password = password;
+        }
+
+
+        public String getUsername() {
+            return username;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+
+}
---
+++ Java_HomeWork_1_1/src/main/java/Task_2_8/starers/ClientStarter2.java
@@ -0,0 +1,9 @@
+package Task_2_8.starers;
+
+import Task_2_8.ClientConnectors.ChatAdapter;
+
+public class ClientStarter2 {
+    public static void main(String[] args) {
+        new ChatAdapter();
+    }
+}
