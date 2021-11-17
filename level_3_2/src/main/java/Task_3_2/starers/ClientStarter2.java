26950936db6cbdf0594943f29e92e4c6d4c4fa41
Ilya <79869848545@mail.ru>
2021-11-17 12:20:20 +0500

    Домашка по 2-му уроку 3-го уровня

---
+++ level_3_2/src/main/java/Task_3_2/starers/ClientStarter2.java
@@ -0,0 +1,9 @@
+package Task_3_2.starers;
+
+import Task_3_2.ClientConnectors.ChatAdapter;
+
+public class ClientStarter2 {
+    public static void main(String[] args) {
+        new ChatAdapter();
+    }
+}
