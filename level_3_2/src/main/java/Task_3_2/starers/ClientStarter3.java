4eae95e26f94c3478ba32932dab3fd52c5bfa1ea
Ilya <79869848545@mail.ru>
2021-11-19 13:31:52 +0500

    11

--- level_3_2/src/main/java/Task_3_2/starers/ClientStarter3.java
+++
@@ -1,9 +0,0 @@
-package Task_3_2.starers;
-
-import Task_3_2.ClientConnectors.ChatAdapter;
-
-public class ClientStarter3 {
-    public static void main(String[] args) {
-        new ChatAdapter();
-    }
-}
